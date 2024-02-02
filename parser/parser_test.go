b936ed201f4cae12343ab5d8319171a2579f08d8
Runek00 <runek00@gmail.com>
2024-02-02 20:15:26 +0100

    parsing integers

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -148,3 +148,36 @@ func TestIdentifierExpression(t *testing.T) {
 			"foobar", ident.TokenLiteral())
 	}
 }
+
+func TestIntegerExpression(t *testing.T) {
+	input := "5;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not ast.IntegerLiteral. got=%T",
+			stmt.Expression)
+	}
+	if ident.Value != 5 {
+		t.Fatalf("ident.Value not %s. got=%d",
+			"5", ident.Value)
+	}
+	if ident.TokenLiteral() != "5" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s",
+			"5", ident.TokenLiteral())
+	}
+}
