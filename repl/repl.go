8554cc4edc3b4b7c01ed71500be80ebcd98a99aa
Runek00 <runek00@gmail.com>
2024-03-08 22:45:55 +0100

    some RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -2,7 +2,7 @@ package repl
 
 import (
 	"Monkey/lexer"
-	"Monkey/token"
+	"Monkey/parser"
 	"bufio"
 	"fmt"
 	"io"
@@ -21,9 +21,21 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
+		program := p.ParseProgram()
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t" + msg + "\n")
 	}
 }
