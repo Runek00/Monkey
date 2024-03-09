be48de0630896e3a4c1ca89202264410b30ffc5c
Runek00 <runek00@gmail.com>
2024-03-09 23:30:10 +0100

    better error message

--- repl/repl.go
+++ repl/repl.go
@@ -35,7 +35,9 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
 	for _, msg := range errors {
-		io.WriteString(out, "\t" + msg + "\n")
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
