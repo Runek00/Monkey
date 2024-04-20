c27984df9158b6c6becb5572b2ff1baf2d80825b
Runek00 <runek00@gmail.com>
2024-04-20 22:49:40 +0200

    some more code to make functions work

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -29,9 +29,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		env.Set(node.Name.Value, val)
 	case *ast.FunctionLiteral:
 		params := node.Parameters
-	body := node.Body
+		body := node.Body
 		return &object.Function{Parameters: params, Env: env, Body: body}
-
 
 	// Expressions
 	case *ast.Identifier:
@@ -64,6 +63,15 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 			return right
 		}
 		return evalInfixExpression(node.Operator, left, right)
+	case *ast.CallExpression:
+		function := Eval(node.Function, env)
+		if isError(function) {
+			return function
+		}
+		args := evalExpressions(node.Arguments, env)
+		if len(args) == 1 && isError(args[0]) {
+			return args[0]
+		}
 	}
 	return nil
 }
@@ -238,4 +246,17 @@ func isTruthy(obj object.Object) bool {
 	default:
 		return true
 	}
+}
+
+func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
+	var result []object.Object
+
+	for _, e := range exps {
+		evaluated := Eval(e, env)
+		if isError(evaluated) {
+			return []object.Object{evaluated}
+		}
+		result = append(result, evaluated)
+	}
+	return result
 }
