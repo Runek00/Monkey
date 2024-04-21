2ea7f05bf2e9ec169bc6b5f74eeff14c7aa2e8ac
Runek00 <runek00@gmail.com>
2024-04-21 23:48:17 +0200

    inner environment

--- object/object.go
+++ object/object.go
@@ -58,15 +58,25 @@ func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
 }
 
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	val, ok := e.store[name]
+	if !ok {
+		val, ok = e.outer.Get(name)
+	}
 	return val, ok
 }
 
